f83fa270d2df9cb239373df24d476506dd3d1b95
scaliaven <132032855+scaliaven@users.noreply.github.com>
2025-02-24 21:14:51 +0000

    Deploying to gh-pages from @ scaliaven/scaliaven.github.io@1c4a33b69dab3b4e8162822cde8e7837e739d5f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,19 +66,19 @@ ninja.data = [{
     },{id: "news-i-become-a-research-assistant-for-professor-shengjie-wang",
           title: 'I become a Research assistant for Professor Shengjie Wang 🎉🎉🎉',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-video-recognition",
+          title: 'Video Recognition',
+          description: "Summer Research Project on Video Popularity Prediction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-audio-classification",
+          title: 'Audio Classification',
+          description: "Machine Learning Final Project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-chemical-reaction-prediction",
+          title: 'Chemical Reaction Prediction',
+          description: "Course Project on Natural Language Processing",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
